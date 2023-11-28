e1a1a97e630380ff3461d5d5df809ef1d9293fe6
mozzam123 <mozzam607@gmail.com>
2023-11-28 10:55:28 +0530

    CallBacks

---
+++ callback.js
@@ -0,0 +1,15 @@
+
+// Callback using function name
+
+setTimeout(function () {
+    console.log('callback as standard function');
+}, 1000)
+
+
+// Callback using arrow functions
+
+setTimeout(() =>{
+    console.log('callback as arrow function');
+}, 1000)
+
+
