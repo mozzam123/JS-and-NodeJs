dd9c3bc2f49d998662bbccfc6872f2765a09ef44
mozzam123 <mozzam607@gmail.com>
2024-02-27 12:52:18 +0530

    Added definition

--- buffer.js
+++ buffer.js
@@ -1,3 +1,9 @@
+// Definition
+"Node.js includes an additional data type called Buffer ( not available in browser's JavaScript )."
+"Buffer is mainly used to store binary data, while reading from a file or receiving packets over the network."
+
+
+
 // Creating a buffer from a hexadecimal string
 const hexBuffer = Buffer.from('1a', 'hex')
 
